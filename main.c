21a67425e591f9167d978d7feabe676db4f12858
o-bachynskyi <138506032+o-bachynskyi@users.noreply.github.com>
2023-07-12 19:20:13 +0300

    Create main.c
    Початкова структура програми

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+// Оголошення структур даних
+
+struct User {
+    // Дані користувача
+};
+
+struct Order {
+    // Дані замовлення
+};
+
+struct Property {
+    // Дані житла
+};
+
+struct Property availableProperties[100];
+
+// Оголошення функцій
+
+void displayMenu() {
+    // Користувацьке меню
+}
+
+void createAccount() {
+    // Створення акаунта
+}
+
+void login() {
+    // Вхід у систему
+}
+
+void viewAccountInfo() {
+    // Виведення інформації про акаунт
+}
+
+void viewOrderHistory() {
+    // Історія замовлень
+}
+
+void viewCurrentOrders() {
+    // Поточні замовлення
+}
+
+void allAvailableProperties() {
+    // Список всіх доступних для оренди житла
+}
+
+void displayAvailableProperties() {
+    // Вивести доступні житла
+}
+
+void searchProperty() {
+    // Пошук житла за певними характеристиками
+}
+
+void viewPropertyDetails(struct Property availableProperties) {
+    // Виведення деталей про житло
+}
+
+void rentProperty() {
+    // Оренда житла
+}
+
+// Головна функція
+
+int main() {
+    
+    return 0;
+}
